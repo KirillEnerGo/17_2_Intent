815ee78e37bcbbf1dff980ff50a2c2661c22b4e1
KirillEnerGo <energykirill.com>
2022-01-15 14:58:25 +0300

    update & lifecycle log

--- app/src/main/java/com/example/a17_2_intent/MainActivity.kt
+++ app/src/main/java/com/example/a17_2_intent/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a17_2_intent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,19 +11,47 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
+		Log.d("LifecycleTest", "MainActivity|onCreate|${hashCode()}")
+
 		startExplicitButton.setOnClickListener {
 
 			val messageText = messageEditText.text.toString()
 
-			// создаем переменную которая будет указывать на класс активити,
+/*			// создаем переменную которая будет указывать на класс активити,
 			// который мы хотим запустить
 			val secondActivityIntent = Intent(
 				this,
 				SecondActivity::class.java // нам необх java класс, а не котлин
 			).putExtra(SecondActivity.KEY_MESSAGE, messageText)
 			// чтобы положить данные в интент метод putExtra
-
 			startActivity(secondActivityIntent)
+*/
+			startActivity(SecondActivity.getIntent(this, messageText))
 		}
+	}
+
+	override fun onStart() {
+		super.onStart()
+		Log.d("LifecycleTest", "MainActivity|onStart|${hashCode()}")
+	}
+
+	override fun onResume() {
+		super.onResume()
+		Log.d("LifecycleTest", "MainActivity|onResume|${hashCode()}")
+	}
+
+	override fun onPause() {
+		super.onPause()
+		Log.d("LifecycleTest", "MainActivity|onPause|${hashCode()}")
+	}
+
+	override fun onStop() {
+		super.onStop()
+		Log.d("LifecycleTest", "MainActivity|onStop|${hashCode()}")
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		Log.d("LifecycleTest", "MainActivity|onDestroy|${hashCode()}")
 	}
 }
--- app/src/main/java/com/example/a17_2_intent/SecondActivity.kt
+++ app/src/main/java/com/example/a17_2_intent/SecondActivity.kt
@@ -1,17 +1,53 @@
 package com.example.a17_2_intent
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Message
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity: AppCompatActivity(R.layout.activity_second) {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		Log.d("LifecycleTest", "SeconActivity|onCreate|${hashCode()}")
 		val message = intent.getStringExtra(KEY_MESSAGE)
 		messageTextView.text = message
 	}
 
+	override fun onStart() {
+		super.onStart()
+		Log.d("LifecycleTest", "SeconActivity|onStart|${hashCode()}")
+	}
+
+	override fun onResume() {
+		super.onResume()
+		Log.d("LifecycleTest", "SeconActivity|onResume|${hashCode()}")
+	}
+
+	override fun onPause() {
+		super.onPause()
+		Log.d("LifecycleTest", "SeconActivity|onPause|${hashCode()}")
+	}
+
+	override fun onStop() {
+		super.onStop()
+		Log.d("LifecycleTest", "SeconActivity|onStop|${hashCode()}")
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		Log.d("LifecycleTest", "SeconActivity|onDestroy|${hashCode()}")
+	}
+
 	companion object{
 		const val KEY_MESSAGE = "message key"
+
+		fun getIntent(context: Context, message: String?): Intent
+		{
+			return Intent(context, SecondActivity::class.java)
+				.putExtra(KEY_MESSAGE, message)
+		}
 	}
 }
